e1ec2a3808a9e7f35046d00a0f3204426409d0ba
Alex Durán <31995317+alex-duran@users.noreply.github.com>
2017-11-03 23:09:27 -0400

    Initial commit

---
+++ scripts/picture-switch.js
@@ -0,0 +1,11 @@
+function imageswap() {
+
+        if (document.getElementById("clicktochange").src == "images/cat1.jpg")
+        {
+            document.getElementById("clicktochange").src = "images/cat2.jpg";
+        }
+        else
+        {
+            document.getElementById("clicktochange").src = "images/cat1.jpg";
+        }
+    }
